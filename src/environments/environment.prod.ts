ddfcc7e93e1841f03db2201994019e87413ae2c5
albsel <alban_selimi@yahoo.com>
2023-01-20 18:30:26 +0100

    clips project finished

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD0vJQtt2iH2pMfXvSuqEW1blF29JvAkE0',
+    authDomain: 'clips-f3c2b.firebaseapp.com',
+    projectId: 'clips-f3c2b',
+    storageBucket: 'clips-f3c2b.appspot.com',
+    appId: '1:322755700561:web:5cd2fa3f07b5e479d85570',
+  },
+};
